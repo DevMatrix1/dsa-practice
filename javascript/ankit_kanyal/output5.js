068a7b854f3d2fafb6e6138e71c1bb36368ef841
Ankit Kanyal <ankitsinghkanyal43@gmail.com>
2022-06-22 15:27:16 +0530

    Solved daily js problems

---
+++ javascript/ankit_kanyal/output5.js
@@ -0,0 +1,18 @@
+class Chameleon {
+  static colorChange(newColor) {
+    this.newColor = newColor;
+    return this.newColor;
+  }
+
+  constructor({ newColor = "green" } = {}) {
+    this.newColor = newColor;
+  }
+}
+
+const freddie = new Chameleon({ newColor: "purple" });
+console.log(Chameleon.colorChange("orange"));
+
+//   A: orange
+//   B: purple
+//   C: green
+//   D: TypeError
