22ba4d84f5321d4796f92dba1fc7953dc2a3eff5
Vedant Bhatnagar <vedant0616@gmail.com>
2022-08-03 21:27:58 +0530

    Devsnest THA and Leetcode problems

--- Python/vedant_bhatnagar/LeetcodeProblems/intersectionLL.py
+++ Python/vedant_bhatnagar/LeetcodeProblems/intersectionLL.py
@@ -22,4 +22,22 @@ class Solution(object):
         
 
         #O(1) space solution
-        
+        #go to end of 1 LL and point it to head of L2
+        #then use LL cycle detection algo
+        ptrA = headA
+        while ptrA.next:
+            ptrA=ptrA.next
+        ptrA.next = headB
+        fast = slow = headA
+        while fast and fast.next:
+            fast = fast.next.next
+            slow=slow.next
+            if fast==slow:
+                fast=headA
+                break
+        if fast and fast.next:
+            while fast!=slow:
+                fast=fast.next
+                slow=slow.next
+            return fast
+        return None
