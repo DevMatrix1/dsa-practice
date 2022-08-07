c796c57ea6ff50485fda98c1f70b4812c8a872c3
sky <aakashjangra2002@gmail.com>
2022-08-07 14:16:36 +0530

    added another approach to valid-palindrome

--- java/sky/LeetCode/07-08-2022/valid-palindrome.java
+++ java/sky/LeetCode/07-08-2022/valid-palindrome.java
@@ -29,3 +29,38 @@ class Solution {
         return true;
     }
 }
+
+//Approach 2: similar to approach 1, haven't made new string. just made the original string toLowerCase() and then skipped non-alphanumeric characters when checking for palindrome 
+class Solution {
+    private boolean alphaNumeric(char c){
+        if((48<=c && c<=57) || (97 <= c && c <= 122))
+            return true;
+        return false;
+    }
+    public boolean isPalindrome(String s) {
+        int n = s.length();
+        s = s.toLowerCase();
+        int front = 0;
+        int back = n - 1;
+        
+        while(front < back){
+            char charf = s.charAt(front);
+            while(front < n-1 && alphaNumeric(s.charAt(front)) == false)
+                front++;
+            
+            while(back > 0 && alphaNumeric(s.charAt(back)) == false)
+                back--;
+            
+            if(front > back)
+                break;
+            
+            
+            if(s.charAt(front) != s.charAt(back))
+                return false;
+            front++;
+            back--;
+        }
+        
+        return true;
+    }
+}
