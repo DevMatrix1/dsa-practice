bbc328afe27b990ce2cfd60cd10b0182b8079977
sky <aakashjangra2002@gmail.com>
2022-08-14 21:05:18 +0530

    a sliding window hard: substring with concat of all words

---
+++ java/sky/devsnest-dsa/heaps/heap_implementation.java
@@ -0,0 +1,18 @@
+public class heap_implementation {
+    
+    private void heapifyDown(int[] arr){
+
+    }
+
+    private void makeHeap(int[] arr){
+        int n = arr.length;
+        for(i = 0; i< n/2; i++){
+            
+        }
+    }
+
+    public void main(String[] args) {
+        int[] nums = {1, 2, 4, 1, 67, 90, 5};
+        makeHeap(nums);
+    }
+}
