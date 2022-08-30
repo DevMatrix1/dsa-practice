b40c499f27e4e054b14c0f82db0c491e4fa3ec43
sky <aakashjangra2002@gmail.com>
2022-08-30 15:06:12 +0530

    made some star and num patterns

--- java/sky/patterns/src/pattern11.java
+++ java/sky/patterns/src/pattern11.java
@@ -3,11 +3,10 @@ public class pattern11 {
         System.out.println("Pattern 11:");
         int n = 5;
         for(int i = 1; i<=n; i++) {
-            for (int j = 1; j < n + i; j++) {
-                if (j <= 5)
-                    System.out.print(" ");
-                else
-                    System.out.print("*");
+            int num = i % 2 == 0? 0: 1;
+            for (int j = 1; j <= i; j++) {
+                System.out.print(num);
+                num = num == 1? 0: 1;
             }
             System.out.println();
         }
