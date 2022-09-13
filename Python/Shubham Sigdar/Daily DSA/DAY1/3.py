94e361e8599d3cdfd740ebf83a5bc0a7895c0666
shubhamsigdar1 <shubhamsigdar@gmail.com>
2022-09-13 11:21:22 +0530

    Day1 challenge

---
+++ Python/Shubham Sigdar/Daily DSA/DAY1/3.py
@@ -0,0 +1,8 @@
+for i in range(5,0,-1):
+    strs='*'*i
+    print(strs)
+# *****
+# ****
+# ***
+# **
+# *
---
+++ Python/Shubham Sigdar/Daily DSA/DAY1/4.py
@@ -0,0 +1,10 @@
+strs=''
+for i in range(1,6):
+    strs=strs+str(i)
+    print(strs)
+
+# 1
+# 12
+# 123
+# 1234
+# 12345
