15c39078742ab932bf7762c0d22a6e6c4d4c45ce
vinit <vinit224488@gmail.com>
2022-09-18 19:49:04 +0530

    armstrongno.

---
+++ Python/vinit/dsa sheet/day-6/basicmath.py
@@ -0,0 +1,13 @@
+#armstrong
+'''n=input()
+q=len(str(n))
+sum=0
+for i in n:
+    ans=int(i)**q 
+    sum=sum+ans 
+if str(sum)==n:
+    print('number is armstrong')
+else:
+    print('number is not armstrong')
+        '''
+
