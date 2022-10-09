c27de9480b27f3fe8f047881a3548969c7983b00
Vinit khandal <vinit224488@gmail.com>
2022-10-09 17:39:09 +0530

    link-list

---
+++ Python/vinit/dsa sheet/day-27/ll.py
@@ -0,0 +1,15 @@
+#Q:https://practice.geeksforgeeks.org/problems/count-nodes-of-linked-list/0?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=count-nodes-of-linked-list
+
+'''def getCount(self, head_node):
+        #code here
+        cur = head_node
+
+        cnt = 0
+
+        while cur != None:
+
+            cnt += 1
+
+            cur = cur.next
+
+        return cnt   '''
