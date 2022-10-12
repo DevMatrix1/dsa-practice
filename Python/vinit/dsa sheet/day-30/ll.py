c4f00f0f305cd31711162316dc5759de019620cf
Vinit khandal <vinit224488@gmail.com>
2022-10-12 09:27:59 +0530

    linklist

---
+++ Python/vinit/dsa sheet/day-30/ll.py
@@ -0,0 +1,15 @@
+#Q:https://practice.geeksforgeeks.org/problems/reverse-a-linked-list/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=reverse-a-linked-list
+
+#   def reverseList(self, head):
+#         # Code here
+#         curr=head
+#         prev=None
+#         while curr!= None:
+#             next= curr.next
+#             curr.next= prev
+#             prev= curr
+#             curr= next
+#         return prev
+            
+
+
