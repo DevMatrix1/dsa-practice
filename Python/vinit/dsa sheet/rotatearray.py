0529704a8bd6eda554a410e6caad7309714edf37
Vinit khandal <111434418+vinit717@users.noreply.github.com>
2022-10-12 10:39:24 +0530

    Create rotatearray.py

---
+++ Python/vinit/dsa sheet/rotatearray.py
@@ -0,0 +1,12 @@
+# # https://leetcode.com/problems/rotate-array/submissions/
+
+
+# class Solution:
+#     def rotate(self, nums: List[int], k: int) -> None:
+#         """
+#         Do not return anything, modify nums in-place instead.
+#         """
+#         while k > 0:
+#             nums.insert(0,nums.pop())
+#             k -= 1
+        
