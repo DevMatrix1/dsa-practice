6ad8716b91d91cab90d1741d2182c564a10420dd
shubhamsigdar1 <shubhamsigdar@gmail.com>
2022-10-28 22:15:43 +0530

    Shubhamsigdar done 7  question of java

---
+++ java/Shubham Sigdar/Function1helloworld.java
@@ -0,0 +1,14 @@
+//Description:
+//        Make a simple function called greet that returns the most-famous "hello world!".
+//
+//        Style Points
+//        Sure, this is about as easy as it gets. But how clever can you be to create the most creative
+//        hello world you can think of? What is a "hello world" solution you would want to show your friends?
+
+
+public class Function1helloworld {
+    // Write a public static function `greet` that returns "hello world!"
+    public static String greet(){
+        return "hello world!";
+    }
+}
