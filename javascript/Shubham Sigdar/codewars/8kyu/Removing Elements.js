6f24e9a12bee5c7b98b343af7473ecf79aa4577a
shubhamsigdar1 <shubhamsigdar@gmail.com>
2022-12-29 15:54:05 +0530

    Adding coding challenge for 29-12-2022

---
+++ javascript/Shubham Sigdar/codewars/8kyu/Removing Elements.js
@@ -0,0 +1,11 @@
+// Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+
+// Example:
+// ["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+
+// None of the arrays will be empty, so you don't have to worry about that!
+function removeEveryOther(arr){
+    //your code here
+    let arr1=arr.filter((e,i)=>i%2==0)
+    return arr1
+  }
