0b0ec23d15a4fab4acbe4483c383865fe46eac96
shubhamsigdar1 <shubhamsigdar@gmail.com>
2022-12-30 06:59:52 +0530

    Adding coding challenge for 30-12-2022

---
+++ javascript/Shubham Sigdar/codewars/8kyu/Grasshopper - Messi Goals.js
@@ -0,0 +1,21 @@
+// Messi's Goal Total
+// Use variables to find the sum of the goals Messi scored in 3 competitions
+
+// Information
+// Messi goal scoring statistics:
+
+// Competition	Goals
+// La Liga	43
+// Champions League	10
+// Copa del Rey	5
+// Task
+// Create these three variables and store the appropriate values using the table above:
+// laLigaGoals
+// championsLeagueGoals
+// copaDelReyGoals
+// Create a fourth variable named totalGoals that stores the sum of all of Messi's goals for this year.
+var laLigaGoals = 43;
+var championsLeagueGoals = 10;
+var copaDelReyGoals = 5;
+
+var totalGoals =  laLigaGoals + championsLeagueGoals + copaDelReyGoals;
