acdf5ab6ad5ef5aa9998cddab4d1026a539c8965
shubhamsigdar1 <shubhamsigdar@gmail.com>
2022-12-31 22:17:43 +0530

    Adding coding challenge for 31-12-2022

---
+++ javascript/Shubham Sigdar/codewars/8kyu/Sum The Strings.js
@@ -0,0 +1,19 @@
+// Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+
+// Example: (Input1, Input2 -->Output)
+
+// "4",  "5" --> "9"
+// "34", "5" --> "39"
+// "", "" --> "0"
+// "2", "" --> "2"
+// "-5", "3" --> "-2"
+// Notes:
+
+// If either input is an empty string, consider it as zero.
+
+// Inputs and the expected output will never exceed the signed 32-bit integer limit (2^31 - 1)
+function sumStr(a,b) {
+    a= +a 
+    b=+b
+    return a+b+'';
+  }
