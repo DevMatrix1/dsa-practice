15e6cf6d277c564125d56c86547410c4dee2c8f9
shubhamsigdar1 <shubhamsigdar@gmail.com>
2023-01-09 10:18:16 +0530

    Adding DSA coding challenge for 09-01-2023

--- javascript/Shubham Sigdar/debugging code/main.js
+++ javascript/Shubham Sigdar/debugging code/main.js
@@ -1,3 +1,21 @@
-
-
-console.log(twoSum([3,2,4],6))
+// Input: strs = ["flower","flow","flight"]
+// Output: "fl"
+// Input: strs = ["dog","racecar","car"]
+// Output: ""
+var longestCommonPrefix = function(strs) {
+    
+    let pre = strs[0];
+    
+    for(let word of strs) {
+                
+        for(let i = pre.length - 1; i >= 0; i--) {
+                             
+            if(pre[i] !== word[i]) {
+                pre = pre.slice(0, i);
+            }
+        }
+    }
+    
+    return pre;
+};
+console.log(longestCommonPrefix(["flower","flow","flight"]))
