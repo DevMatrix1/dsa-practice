39ebc1d836da811b19dfbcedc59695f64a37a357
shubhamsigdar1 <shubhamsigdar@gmail.com>
2023-01-11 23:19:57 +0530

    Adding ds coding challenge for 11-01-2023

--- javascript/Shubham Sigdar/Frontend Challenge by sadananadpai/4/main.js
+++ javascript/Shubham Sigdar/Frontend Challenge by sadananadpai/4/main.js
@@ -4,8 +4,12 @@ let topBottom =document.querySelector('#tb');
 let sucErr=document.querySelector('#sewi');
 let duration=document.querySelector('#duration');
 let btn =document.querySelector('button');
-
-
+let cont=document.querySelector('.container');
+let sum1=0;
+let sum2=0;
+let sum3=0;
+let sum4=0;
+let i=0;
 btn.addEventListener('click',()=>{
     let leftRightValue=leftRight.value;
     // console.log(leftRightValue)
@@ -14,6 +18,35 @@ btn.addEventListener('click',()=>{
     let sucErrValue=sucErr.value;
     // console.log(sucErrValue);
     let durationValue=duration.value;
-    // console.log(durationValue);
+    console.log(durationValue);
+    let div = document.createElement("div");
+    div.className="dynamicdiv"
     
+    if(leftRightValue==='left' && topBottomValue==='top'){
+        // div.style.top='0px'
+        div.style.left='0px';
+        div.style.top=`${sum1}px`;
+        div.innerText=i++;
+        sum1+=35;
+        document.body.prepend(div);
+        // leftTopRemove();
+    }else if(leftRightValue==='left' && topBottomValue==='bottom'){
+        // div.style.top='0px'
+        div.style.left='0px';
+        div.style.bottom=`${sum2}px`;
+        sum2+=35;
+    }else if(leftRightValue==='right' && topBottomValue==='top'){
+        div.style.right='0px';
+        div.style.top=`${sum3}px`;
+        sum3+=35;
+    }else{
+        div.style.right='0px';
+        div.style.bottom=`${sum4}px`;
+        sum4+=35;
+
+    }
+
+    // document.body.insertBefore(div,cont);
 })
+
+
