ef9d1ca3a5df051b77bbab7b82812d43599e4277
shubhamsigdar1 <shubhamsigdar@gmail.com>
2023-01-18 11:18:15 +0530

    Adding dsa coding challenge for 18-01-2023

---
+++ javascript/Shubham Sigdar/Neetcode/Can Place Flowers.js
@@ -0,0 +1,28 @@
+// You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+
+// Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule.
+
+ 
+
+// Example 1:
+
+// Input: flowerbed = [1,0,0,0,1], n = 1
+// Output: true
+// Example 2:
+
+// Input: flowerbed = [1,0,0,0,1], n = 2
+// Output: false
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function(flowerbed, n) {
+    for(let i=0;i<flowerbed.length;i++){
+      if(flowerbed[i]===0 && flowerbed[i-1]!=1 && flowerbed[i+1]!=1 ){
+        n--;
+        flowerbed[i]=1
+      }
+    }
+    return n>0?false:true
+  };
