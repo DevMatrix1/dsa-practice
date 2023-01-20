a06c0062b47a22bf546708fb2a5fe19ca57e171a
shubhamsigdar1 <shubhamsigdar@gmail.com>
2023-01-20 15:09:03 +0530

    Adding dsa coding challenge for 20-01-2023

---
+++ javascript/Shubham Sigdar/Neetcode/Next Greater Element I.js
@@ -0,0 +1,57 @@
+// The next greater element of some element x in an array is the first greater element that is to the right of x in the same array.
+
+// You are given two distinct 0-indexed integer arrays nums1 and nums2, where nums1 is a subset of nums2.
+
+// For each 0 <= i < nums1.length, find the index j such that nums1[i] == nums2[j] and determine the next greater element of nums2[j] in nums2. If there is no next greater element, then the answer for this query is -1.
+
+// Return an array ans of length nums1.length such that ans[i] is the next greater element as described above.
+
+ 
+
+// Example 1:
+
+// Input: nums1 = [4,1,2], nums2 = [1,3,4,2]
+// Output: [-1,3,-1]
+// Explanation: The next greater element for each value of nums1 is as follows:
+// - 4 is underlined in nums2 = [1,3,4,2]. There is no next greater element, so the answer is -1.
+// - 1 is underlined in nums2 = [1,3,4,2]. The next greater element is 3.
+// - 2 is underlined in nums2 = [1,3,4,2]. There is no next greater element, so the answer is -1.
+// Example 2:
+
+// Input: nums1 = [2,4], nums2 = [1,2,3,4]
+// Output: [3,-1]
+// Explanation: The next greater element for each value of nums1 is as follows:
+// - 2 is underlined in nums2 = [1,2,3,4]. The next greater element is 3.
+// - 4 is underlined in nums2 = [1,2,3,4]. There is no next greater element, so the answer is -1.
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var nextGreaterElement = function(nums1, nums2) {
+    let nums=[];
+     for(let i=0;i<nums1.length;i++){
+       let nums11=arr(nums1[i],nums2)
+       if(nums11==undefined){
+         nums.push(-1)
+       }else{
+         nums.push(nums11)
+       }
+       
+     }
+   return nums
+   
+ };
+ 
+ function arr(n,nums2){
+    let next = -1;
+     for(let i=0;i<nums2.length;i++){
+       if(n==nums2[i]){
+           for (j = i + 1; j < nums2.length; j++){
+             if (nums2[i] < nums2[j]){
+               return(nums2[j]);
+             }
+          }
+       }
+     }
+ }
